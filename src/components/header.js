fb26ceb1b032b62526ce9d336fac34416497d452
VaStan96 <vastan96@gmail.com>
2024-11-20 15:15:28 +0100

    3. test

---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Header() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // localStorage.getItem("authToken");
+        localStorage.removeItem("authToken");
+        navigate("/");
+    };
+
+    return (
+        <header>
+        <nav>
+            <ul>
+            <li>
+                <Link to="/">Login</Link>
+            </li>
+            <li>
+                <Link to="/tasks">Tasks</Link>
+            </li>
+            <li>
+                <Link to="/tasks/new">Add Task</Link>
+            </li>
+            <li>
+                <Link to="/notifications">Notifications</Link>
+            </li>
+            </ul>
+        </nav>
+        </header>
+    );
+}
+
+export default Header;
